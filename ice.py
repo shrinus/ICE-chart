34a52f1ac07c36341357e12e143ea624ced67b4f
Shrinus <srinivasan2003@outlook.com>
2022-08-22 19:17:18 -0700

    compact

--- ice.py
+++ ice.py
@@ -1,11 +1,4 @@
-#EQ = input("Enter the chemical equation\n ")
-EQ = "AgNO3 + 2CaO = Ag2O + Ca(NO3)2"
-
-
-#EQ = [EQ.split(" = ")[i].split(" + ") for i in range(2)]
-
-#EQ = [[[1, j] if j[0].isalpha() else [j[0], j[1:]] for j in i] for i in EQ]
-
-EQ = [[[1, j] if j[0].isalpha() else [j[0], j[1:]] for j in EQ.split(" = ")[i].split(" + ")] for i in range(2)]
-
-print(EQ)
+EQ = input("\n\nEnter the chemical equation. Surround \"+\" and \"+\" with spaces. ex: AgNO3 + 2CaO = Ag2O + Ca(NO3)2\n ")
+K = input("\nEnter the equilibrium constant\n ")
+EQ = [[[1, j] if j[0].isalpha() else [j[0], j[1:]] for j in i.split(" + ")] for i in EQ.split(" = ")]
+[[j.append(input("\nWhat is the concentration of " + j[1] + "? ")) for j in i] for i in EQ]
