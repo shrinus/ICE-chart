a97b891cf880b4e4b8e3ce9e35a43014ab846d0c
Shrinus <srinivasan2003@outlook.com>
2022-08-22 18:09:22 -0700

    logic

--- ice.py
+++ ice.py
@@ -1,7 +1,11 @@
 #EQ = input("Enter the chemical equation\n ")
-EQ = "AgNO3 + NaCl = AgCl + NANO3"
+EQ = "AgNO3 + 2CaO = Ag2O + Ca(NO3)2"
 
 
-EQ.split(" = ")
+#EQ = [EQ.split(" = ")[i].split(" + ") for i in range(2)]
 
-print(EQ.split(" = ")[0].split(" + "))
+#EQ = [[[1, j] if j[0].isalpha() else [j[0], j[1:]] for j in i] for i in EQ]
+
+EQ = [[[1, j] if j[0].isalpha() else [j[0], j[1:]] for j in EQ.split(" = ")[i].split(" + ")] for i in range(2)]
+
+print(EQ)
