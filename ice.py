98c5977bff4afa8cbfa1cc755ffba751ed26b9bf
Shrinus <srinivasan2003@outlook.com>
2022-08-24 19:08:27 -0700

    bugfix

--- ice.py
+++ ice.py
@@ -1,26 +1,22 @@
 from functools import reduce
 
 def main():
-    EQ = input("\nEnter the chemical equation. ex: CaCl2 = Ca2+ + 2Cl-\n")
-    K = float(input("\nEnter the equilibrium constant\n"))
+    EQ = input("\nWhat is the chemical equation? ex: CaCl2 = Ca2+ + 2Cl- ")
+    K = float(input("\nWHat is the equilibrium constant? "))
 
-    EQ = [[[1, j] if j[0].isalpha() else [int(j[0]), j[1:]] \
-        for j in i.split(" + ")] for i in EQ.split(" = ")]
-    EQ = [[j.append(float(input("What is the concentration of " + j[1] + "?"))) \
-        for j in i] for i in EQ]
+    EQ = [[[1, j] if j[0].isalpha() else [int(j[0]), j[1:]] for j in i.split(" + ")] for i in EQ.split(" = ")]
+    [[j.append(float(input("\nWhat is the concentration of " + j[1] + "? "))) for j in i] for i in EQ]
 
     i = min([j[2] for j in EQ[0]] + [j[2] for j in EQ[1]]) / 2
     print("\n", newton(lambda x: q(EQ, K, x), i))
 
 def newton(f, i, h = 0.0000000001):
-    """finds zeroes of f starting at some initial value i"""
     while abs(f(i)) > h:
         i -= (f(i) * h) / (f(i + h) - f(i))
 
     return i
 
 def q(EQ, K, n):
-    """measures difference between Q and K"""
     R = reduce(lambda x, y: x * y, [(j[2] - j[0] * n) ** j[0] for j in EQ[0]])
     P = reduce(lambda x, y: x * y, [(j[2] + j[0] * n) ** j[0] for j in EQ[1]])
 
